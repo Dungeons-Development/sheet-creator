131ebe764170a5e19aff2d093fb05da6ed320ec0
ChrisMeyer7088 <chrismeyer175@gmail.com>
2021-03-01 08:22:55 -0800

    Fixed package issue, removed document.body rendering console error

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,4 +3,4 @@ import { Main } from './components/main.jsx';
 
 import './sharedCss/global.scss'; // Globally applied CSS
 
-render(<Main />, document.body);
+render(<Main />, document.getElementById('root'));
