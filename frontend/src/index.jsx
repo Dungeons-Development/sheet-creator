fde1925d20de07902f108465113511b678d70727
mbotsko7 <mbotsko7@gmail.com>
2021-03-07 09:56:44 -0800

    add ionic packages and styles

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,11 @@
 import { render } from 'react-dom';
 import { Main } from './components/main.jsx';
 
+import '@ionic/react/css/core.css';
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
 import './sharedCss/global.scss'; // Globally applied CSS
 
 render(<Main />, document.getElementById('root'));
