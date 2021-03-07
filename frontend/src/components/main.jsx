f319d3ef9eb357edfd77af61fd7b9ce5ef806935
mbotsko7 <mbotsko7@gmail.com>
2021-03-07 11:56:41 -0800

    lint fixes

--- frontend/src/components/main.jsx
+++ frontend/src/components/main.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import styles from './main.scss';
 
-import { TinyMCE } from './tinymce.jsx'
-import { Renderer } from './renderer.jsx'
+import { TinyMCE } from './tinymce.jsx';
+import { Renderer } from './renderer.jsx';
 
+/* eslint arrow-body-style: 0 */
 export const Main = () => {
+  /* eslint no-unused-vars: 0 */
   const [sheetHtml, setSheetHtml] = useState('<h1>Hi there!</h1><div>What are you up to?</div>');
   const [rendererOptions, setRendererOptions] = useState({});
 
--- frontend/src/components/tinymce.jsx
+++ frontend/src/components/tinymce.jsx
@@ -3,26 +3,23 @@ import PropTypes from 'prop-types'
 
 export const TinyMCE = ({ html, setHtml }) => {
   return (
-    <div>
-      <button>HTML</button>
-      <Editor
-        initialValue={html}
-        init={{
-          height: 500,
-          menubar: false,
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount code'
-          ],
-          toolbar:
-            'undo redo | formatselect | bold italic backcolor | \
-            alignleft aligncenter alignright alignjustify | \
-            bullist numlist outdent indent | removeformat | help | code'
-        }}
-        onEditorChange={setHtml}
-      />
-    </div>
+    <Editor
+      initialValue="<p>This is the initial content of the editor</p>"
+      init={{
+        height: 500,
+        menubar: false,
+        plugins: [
+          'advlist autolink lists link image charmap print preview anchor',
+          'searchreplace visualblocks code fullscreen',
+          'insertdatetime media table paste code help wordcount',
+        ],
+        toolbar:
+          'undo redo | formatselect | bold italic backcolor | '
+          + 'alignleft aligncenter alignright alignjustify |'
+          + 'bullist numlist outdent indent | removeformat | help',
+      }}
+      onEditorChange={editorChange}
+    />
   );
 }
 
